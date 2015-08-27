c9de88d98361570b96a2a65acb2cdb90a489dc29
Jose M. Gomez <jm.gomez@ub.edu>
2015-08-27 10:56:30 +0200

    Added the licenses

--- GMPSwiftTests/IntMPTests.swift
+++ GMPSwiftTests/IntMPTests.swift
@@ -1,10 +1,32 @@
-//
-//  GMPSwiftTests.swift
-//  GMPSwiftTests
-//
-//  Created by José María Gómez Cama on 26/8/15.
-//  Copyright (c) 2015 José María Gómez Cama. All rights reserved.
-//
+/* IntMP tests for GNU multiple precision over Switch.   -*- mode: c -*-
+
+Copyright 2015 Jose Maria Gomez Cama
+
+This file is part of the GMPSwitch Library.
+
+The GMPSwitch Library is free software; you can redistribute it and/or modify
+it under the terms of either:
+
+* the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+or
+
+* the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any
+later version.
+
+or both in parallel, as here.
+
+The GMPSwitch Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received copies of the GNU General Public License and the
+GNU Lesser General Public License along with the GNU MP Library.  If not,
+see https://www.gnu.org/licenses/.  */
 
 import Cocoa
 import XCTest
