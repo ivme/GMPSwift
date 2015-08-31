8a687e12294b8cd3a0275bb049c5e88b4aefa981
Jose M. Gomez <jm.gomez@ub.edu>
2015-08-31 19:34:16 +0200

    Trying to make the Framework installable.

--- Integer.playground/Contents.swift
+++ Integer.playground/Contents.swift
@@ -1,16 +1,8 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
 import GMPSwift
 
 var str = "Hello, playground"
 
-let i: Int = 13
-let j: Int = 5
 
-i.distanceTo(j)
-
-i << 3
-i << -3
-
-
+var a = IntMP()
