7b83cbc532fe2abe8a2194445a6e2badbab05806
Jose M. Gomez <jm.gomez@ub.edu>
2015-09-03 22:10:03 +0200

    Creating a test application to check if it is possible to upload the framework.

---
+++ TestGMPSwift/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  TestGMPSwift
+//
+//  Created by José María Gómez Cama on 3/9/15.
+//  Copyright (c) 2015 José María Gómez Cama. All rights reserved.
+//
+
+import Foundation
+import Swift
+import GMPSwift
+
+println("Hello, World!")
+
+var mp = IntMP(1)
+
+println(mp.description)
+
+
